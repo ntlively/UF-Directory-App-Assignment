1f3e4067e9adb303be8b03f00f69f0b0587f30ab
Nathan Lively <ntlively@ufl.edu>
2017-08-30 20:57:36 -0400

    Search Functionality
    Successfully display directory of all buildings, and filter the display based of the search input.  Works for both code and name, ignores type case.

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ angular.module('listings', []);
 
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('directoryApp', ['listings']);
+
+//https://stackoverflow.com/questions/12046928/how-to-use-regex-with-ng-repeat-in-angularjs
+//substantially modified to work with the search term and with a set regular expression
+app.filter('regex', function() {
+    return function(input, field, scope) {
+        if(scope.searchTerm != ''){
+            var reg = scope.searchTerm.toLowerCase() + "+";
+        }else{
+            var reg = ".+";
+        }
+        var patt = new RegExp(reg);      
+        var out = [];
+        for (var i = 0; i < input.length; i++){
+            if(patt.test(input[i][field[0]].toLowerCase()) || patt.test(input[i][field[1]].toLowerCase()))
+                out.push(input[i]);
+        }      
+      return out;
+    };
+  });
--- listingController.js
+++ listingController.js
@@ -2,13 +2,16 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
+    $scope.searchTerm = "";
+    $scope.currentDetails ="";
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {};
     $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+      $scope.currentDetails=index;
+    };
   }
 ]);
